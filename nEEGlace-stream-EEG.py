1726e8e39a4bce4d50da0af174272637052f3fd7
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-05-03 15:36:16 +0200

    Update nEEGlace-stream-EEG.py
    Resolved issue with the updation of incoming stream

--- nEEGlace-stream-EEG.py
+++ nEEGlace-stream-EEG.py
@@ -1,52 +1,136 @@
-# libraries 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from pylsl import StreamInlet, resolve_stream
+import math
+import pylsl
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication
+from pyqtgraph.Qt import QtCore
+from typing import List
+import sys
+
+# parameters for the plotting window
+# time period to show (in ms)
+plotPeriod = 5  
+# interval period between screen updates (in ms)
+updateInterval = 60 
+# interval period of pulling new data (in ms)
+pullInterval = 500
 
 
-# printing status message
-print("Looking for an EEG stream...")
-# resolve and initialise EEG stream using LSL
-streams = resolve_stream('type', 'EEG')  
-inlet = StreamInlet(streams[0])
-# retriving info from stream info
-nchans = inlet.info().channel_count()
-fs = inlet.info().nominal_srate()
-# printing status message
-print('EEG Stream Found...')
-print('Stream Info:')
-print(f'Channels: {nchans}, Sampling rate: {fs}')
+# class to represent a plottable inlet
+class Inlet:
+    # constructor of Inlet
+    def __init__(self, info: pylsl.StreamInfo):
+        # creating inlet: 
+        # max_buflen set to plot duration so that old data is discarded)
+        # applied online click sync to make all streams in same time domain as local lsl_clock()
+        self.inlet = pylsl.StreamInlet(info, max_buflen=plotPeriod, processing_flags=pylsl.proc_clocksync | pylsl.proc_dejitter)    
+        # store the stream info
+        self.name = info.name()
+        self.nchan = info.channel_count()
+    
+    # function to pull data from inlet and plot it  
+    def pullPlot(self, plotTime: float, plt: pg.PlotItem):
+        # plot_time - lowest timestamp visible in plot
+        # plt - plot the data should be shown on  
+        pass
 
-# initialise figure window
-fig, axes = plt.subplots(nchans, 1, figsize=(10, 6))
-# time window to display in the plot (in ms)
-timewindow = 10 
-# converting to samples 
-timesamples = int(timewindow * fs)
-# to store EEG data for each channel
-eegdata = np.zeros((timesamples, nchans))
 
-# initialize plot lines
-lines = []
-for i, ax in enumerate(axes):
-    line, = ax.plot(np.linspace(0, timewindow, timesamples), np.zeros(timesamples), lw= 1)
-    lines.append(line)
-    ax.set_xlim(0, timewindow)
-    ax.set_ylim(-500, 500)  
+# class to handle multi-channel data
+class DataInlet(Inlet):
+    dtypes = [[], np.float32, np.float64, None, np.int32, np.int16, np.int8, np.int64]    
+    # constructor of DataInlet
+    def __init__(self, info: pylsl.StreamInfo, plt: pg.PlotItem):
+        # calling the constructor of the super class
+        super().__init__(info)
+        # calculating size for buffer
+        bufsize = (2 * math.ceil(info.nominal_srate() * plotPeriod), info.channel_count())
+        # initialising the buffer
+        self.buffer = np.empty(bufsize, dtype=self.dtypes[info.channel_format()])
+        empty = np.array([])
+        # creating curve object for each channel that will handle displaying the data
+        self.curves = [pg.PlotCurveItem(x=empty, y=empty, autoDownsample=True) for _ in range(self.nchan)]
+        for curve in self.curves:
+            # adding each curve to the plt
+            plt.addItem(curve)
+    
+    # function to pull new data chunks from a data stream, update the data buffer
+    # and update the corresponding plots for each channel of data. 
+    def pullPlot(self, plotTime, plt):
+        # pull new chunk of data from the data stream
+        _, ts = self.inlet.pull_chunk(timeout=0.0, max_samples= self.buffer.shape[0], dest_obj= self.buffer)
+        if ts:
+            # converting timestamps to numpy
+            ts = np.asarray(ts)
+            vals = self.buffer[0:ts.size, :]
+            new_ts = None
+            oldOffset = 0
+            newOffset = 0
+            for ichan in range(self.nchan):
+                # retrieve current plot data (old timestamps and values)
+                old_ts, old_vals = self.curves[ichan].getData()
+                if ichan == 0:
+                    # calculating the portion of the old data that remains visible given the new plot time
+                    oldOffset = old_ts.searchsorted(plotTime)
+                    # same for the new data, in case we pulled more data than
+                    # can be shown at once
+                    newOffset = ts.searchsorted(plotTime)
+                    # append new timestamps to the trimmed old timestamps
+                    new_ts = np.hstack((old_ts[oldOffset:], ts[newOffset:]))
+                # append new data to the trimmed old data
+                new_vals = np.hstack((old_vals[oldOffset:], vals[newOffset:, ichan] - ichan))
+                # replace the old data
+                self.curves[ichan].setData(new_ts, new_vals)
 
-# function to update plot
-def updatePlot(frame):
-    global eegdata
-    # get new sample
-    sample, timestamp = inlet.pull_sample()
-    if sample:
-        eegdata = np.roll(eegdata, -1, axis= 0)
-        eegdata[-1, :] = sample
-    for i, line in enumerate(lines):
-        line.set_ydata(eegdata[:, i])
-    return lines
+def main():
+    inlets: List[Inlet] = []
+    print("Looking for nEEGlace...")
+    # resolving streams
+    streams = pylsl.resolve_streams()
 
-# plotting 
-ani = FuncAnimation(fig, updatePlot, blit= True, interval= 1)
-plt.show()
+    # setting up plot window
+    pw = pg.plot(title='nEEGlace EEG Stream')
+    plt = pw.getPlotItem()
+    plt.enableAutoRange(x=False, y=True)
+
+    # iterate over found streams, creating specialized inlet objects that will
+    # handle plotting the data
+    for info in streams:
+        if info.nominal_srate() != pylsl.IRREGULAR_RATE \
+                and info.channel_format() != pylsl.cf_string:
+            print('Connected to nEEGlace')
+            inlets.append(DataInlet(info, plt))
+        else:
+            print('nEEGlace not detected')
+
+    # function to scroll the plot window along x-axis
+    def scroll():
+        fudgeFactor = pullInterval * .002
+        plotTime = pylsl.local_clock()
+        pw.setXRange(plotTime - plotPeriod + fudgeFactor, plotTime - fudgeFactor)
+
+    # function to update the data 
+    def update():
+        mintime = pylsl.local_clock() - plotPeriod
+        # call pullPlot for each inlet.
+        for inlet in inlets:
+            inlet.pullPlot(mintime, plt)
+
+    # create a timer that will move the view every update interval
+    updateTimer = QtCore.QTimer()
+    updateTimer.timeout.connect(scroll)
+    updateTimer.start(updateInterval)
+
+    # create a timer that will pull and add new data occasionally
+    pullTimer = QtCore.QTimer()
+    pullTimer.timeout.connect(update)
+    pullTimer.start(pullInterval)
+
+    
+
+    # Start Qt event loop unless running in interactive mode or using pyside.
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QApplication.instance().exec_()
+
+
+if __name__ == '__main__':
+    main()
