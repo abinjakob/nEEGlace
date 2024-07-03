86dbfcff071b0b71af55d99eebeb70eeecbf15f0
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-07-03 16:25:08 +0200

    Update BellaBoard_OnsetDetector.cpp

--- BellaBoard_OnsetDetector.cpp
+++ BellaBoard_OnsetDetector.cpp
@@ -13,29 +13,41 @@ the energyThreshold it activates the digital pin if it is not in the refractory 
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <libraries/AudioFile/AudioFile.h>
+#include <libraries/Biquad/Biquad.h>
+#include <algorithm>
+
 
 // config file 
 const std::string configFilename = "config.txt"; 
 
 // configuration parameters
-int bufferSize = 512; 						// num of samples stored in buffer for energy calculation
-int hopSize = 256;			 				// interval at which buffer is processed
-float refractoryPeriod = 0.5;				// refractory period (sec)
-float activationDuration = 0.05;			// duration to activate the digital output pin after onset detection (sec)
-int inputGain = 55;							// input gain for the microphones 	
-float energyThreshold = 0.2;				// threshold for detecting an onset
+int bufferSize = 512; 							// num of samples stored in buffer for energy calculation
+int hopSize = 256;			 					// interval at which buffer is processed
+float refractoryPeriod = 0.5;					// refractory period (sec)
+float activationDuration = 0.05;				// duration to activate the digital output pin after onset detection (sec)
+float inputGain = 55;								// input gain for the microphones 	
+float energyThreshold = 0.2;					// threshold for detecting an onset
 float toneFreq = 200.0;
 
 // state variables
 std::vector<float> audioBuffer(bufferSize, 0.0f);
-int bufferIndex = 0;						// current position in audioBuffer
-int hopCounter = 0;							// to track samples processed since last energy calculation 
+int bufferIndex = 0;							// current position in audioBuffer
+int hopCounter = 0;								// to track samples processed since last energy calculation 
 int refractoryPeriodSamples;
-int refractoryCounter = 0;					// to track refractory period
-int timeSinceLastOnset = 0; 				// to track samples since last onset
+int refractoryCounter = 0;						// to track refractory period
+int timeSinceLastOnset = 0; 					// to track samples since last onset
 int activationDurationSamples;
-int activationCounter = 0;					// to track how long digital output is activated
-const int digitalPin = 0;					// digital pin number to activate
+int activationCounter = 0;						// to track how long digital output is activated
+const int digitalPin = 0;						// digital pin number to activate
+
+// recording audio variables
+std::vector<std::vector<float>> audioRecorder;	// variable to save
+std::string audioFilename = "audiofile.wav";	// filename to save audio
+double recordAudio = 0;						// recording duration in sec 
+double recordDuration = 20;						// recording duration in sec 
+unsigned int recordingFrames = 0;				// to track the frames recorded
+
 
 // function to read from config file 
 bool readConfig(const std::string& filename){
@@ -53,13 +65,36 @@ bool readConfig(const std::string& filename){
 		std::istringstream iss(line);
 		std::string key;
 		// looking for energy threshold key in config 
-		if(iss >> key && key == "energyThreshold"){
-			// read and parse energy threshold value
-			if(!(iss >> energyThreshold)){
-				rt_printf("Error reading energy threshold value\n");
-				return false;
+		if(iss >> key){
+			if(key == "energyThreshold"){
+				// read and parse energy threshold value
+				if(!(iss >> energyThreshold)){
+					rt_printf("Error reading energy threshold value\n");
+					return false;
+				}
+			// looking for record on/off key in config 
+			} else if(key == "recordAudio"){
+				// read and parse record duration value
+				if(!(iss >> recordAudio)){
+						rt_printf("Error reading recording on/off value\n");
+					return false;
+				}
+			// looking for record duration key in config
+			} else if(key == "recordDuration"){
+				// read and parse record duration value
+				if(!(iss >> recordDuration)){
+						rt_printf("Error reading recording duration value\n");
+					return false;
+				}
+			// looking for input gain key in config
+			}else if(key == "inputGain"){
+				// read and parse record duration value
+				if(!(iss >> inputGain)){
+						rt_printf("Error reading input gain value\n");
+					return false;
+				}
 			}
-		}
+		} 
 	}
 	
 	// closing config file
@@ -92,10 +127,31 @@ bool setup(BelaContext *context, void *userData) {
 	// read config file 
 	if(!readConfig(configFilename)){
 		rt_printf("Using default energy threshold value: %f\n", energyThreshold);
+		rt_printf("Using default recording duration value: %f\n", recordDuration);
+		rt_printf("Using default input gain value: %f\n", inputGain);
 	} else {
 		rt_printf("Using energy threshold value from config file: %f\n", energyThreshold);
+		rt_printf("Using recording duration value from config file: %f\n", recordDuration);
+		rt_printf("Using input gain value from config file: %f\n", inputGain);
 	}
 	
+	// audio recording status
+	if(recordAudio == 1){
+		rt_printf("Audio recording is ON\n");
+	} else if(recordAudio == 0){
+		rt_printf("Audio recording is OFF\n");
+	}
+	
+	// allocate memory to store audio
+	unsigned int numFrames = context->audioSampleRate * recordDuration;
+	audioRecorder.resize(context->audioInChannels);
+	try {
+		for(auto& c : audioRecorder)
+			c.resize(numFrames);
+	} catch (std::exception& e){
+		fprintf(stderr, "Error while allocating memory.");
+		return false;
+	}
 	return true;
 }
 
@@ -108,6 +164,7 @@ void render(BelaContext *context, void *userData) {
 	for (unsigned int n = 0; n < context->audioFrames; ++n) {
 		// read the current sample from the input channel 0
 		float currentSample = audioRead(context, n, 0);
+		
 	    // update the buffer with the current sample
 	    audioBuffer[bufferIndex] = currentSample;
 	    bufferIndex = (bufferIndex + 1) % bufferSize;
@@ -134,10 +191,6 @@ void render(BelaContext *context, void *userData) {
 	            activationCounter = activationDurationSamples;       // set the activation counter
 	            digitalWrite(context, n, digitalPin, HIGH);          // activate the digital output pin
 	            // rt_printf("Pin On\n");
-	            
-	            //audioWrite(context, n, 0, out);
-	            //audioWrite(context, n, 1, out);
-	            
 	        }
 	    }
 	    
@@ -155,9 +208,6 @@ void render(BelaContext *context, void *userData) {
 	        if (activationCounter == 0) {
 	            digitalWrite(context, n, digitalPin, LOW);			  // deactivate the digital output pin
 	            // rt_printf("Pin Off\n");
-	            
-	            //audioWrite(context, n, 0, out);
-	        	//audioWrite(context, n, 1, out);
 	        }
 	        
 	        // send audio pulse to audio output pin when trigger detected
@@ -172,7 +222,24 @@ void render(BelaContext *context, void *userData) {
 			audioWrite(context, n, 0, 0.0f);
 	        audioWrite(context, n, 1, 0.0f);
 		}
+		
+		if(recordAudio == 1){
+			// store current sample to record
+			audioRecorder[0][recordingFrames] = currentSample;
+			++recordingFrames;
+			if(recordingFrames >= audioRecorder[0].size()){
+				Bela_requestStop();
+				return;
+			}
+		}
 	}
 }
 
-void cleanup(BelaContext *context, void *userData) {}
+void cleanup(BelaContext *context, void *userData) {
+	for(auto& i : audioRecorder)
+		i.resize(recordingFrames);
+	if(recordAudio == 1){
+		AudioFileUtilities::write(audioFilename, audioRecorder, context->audioSampleRate);
+		rt_printf("Audio recording saved\n");
+	}
+}
