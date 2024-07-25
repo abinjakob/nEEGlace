d69dcf26b6af1e2cf64ab55fd451424f3fe413e6
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-07-25 13:01:27 +0200

    added Stream Module UI

--- nEEGlaceGUI_v1.py
+++ nEEGlaceGUI_v1.py
@@ -41,6 +41,7 @@ H2 = ('Arial', 20, 'bold')
 H3 = ('Arial', 16, 'bold')
 B1 = ('Arial', 14)
 B2 = ('Arial', 12)
+B3 = ('Arial', 10)
 
 # configure grid layout 
 app.grid_rowconfigure(0, weight=1)
@@ -102,7 +103,7 @@ title = customtkinter.CTkLabel(mainFrame, text= 'Welcome to nEEGlace', font=H1)
 title.grid(row=0, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (40,0))
 # body text '
 bodystr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua.'
-body = customtkinter.CTkLabel(mainFrame, text= bodystr, font=B1, text_color='#979797', justify= 'left')
+body = customtkinter.CTkLabel(mainFrame, text= bodystr, font=B2, text_color='#979797', justify= 'left')
 body.grid(row=2, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (10,0))
 
 # troubleshoot button
@@ -276,9 +277,20 @@ t2_BTnext.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
 def on_cfgmsave():
     configFrameMain.grid_forget()
     mainFrame.grid(sticky='nsew')
+def cfgM_recordtoggleEvent():
+    if cfgM_recordtoggleVar.get() == 1:
+        cfgM_durentry.configure(state= 'normal', border_color= '#565b5e', fg_color= '#343638')
+        cfgM_recordtoggle.configure(text= 'ON')
+    if cfgM_recordtoggleVar.get() == 0:
+        cfgM_durentry.configure(state= 'disabled', border_color= '#343638', fg_color= '#2b2b2b')
+        cfgM_recordtoggle.configure(text= 'OFF')
+        
 def on_cfgsave():
     print(f'Gain      : {cfgM_gainentry.get()}')
     print(f'Threshold : {cfgM_threshentry.get()}')
+    if cfgM_recordtoggleVar.get() == 1:
+        print(f'Record Duration : {cfgM_durentry.get()}')
+        
 
 # title 
 cfgM_title =  customtkinter.CTkLabel(configFrameMain, text= 'Configure nEEGlace', font=H2)
@@ -290,24 +302,49 @@ cfgM_title1.grid(row=1, column=0, columnspan= 5, sticky='w', padx= (40,0), pady=
 cfgM_gaintxt = customtkinter.CTkLabel(configFrameMain, text= 'Microphone Input Gain', font=B1)
 cfgM_gaintxt.grid(row=2, column=0, sticky='w', padx= (40,0), pady= (10,0))
 cfgM_gainentry = customtkinter.CTkEntry(configFrameMain, width= 48)
-cfgM_gainentry.grid(row=2, column=2, sticky='w', padx= (0,0), pady= (10,0))
+cfgM_gainentry.grid(row=2, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # energy threshold
 cfgM_threshtxt = customtkinter.CTkLabel(configFrameMain, text= 'Energy Threshold', font=B1)
 cfgM_threshtxt.grid(row=3, column=0, sticky='w', padx= (40,0), pady= (0,0))
 cfgM_threshentry = customtkinter.CTkEntry(configFrameMain, width= 48)
-cfgM_threshentry.grid(row=3, column=2, sticky='w', padx= (0,0), pady= (10,0))
+cfgM_threshentry.grid(row=3, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # audio setings
 cfgM_title2 = customtkinter.CTkLabel(configFrameMain, text= 'Audio Recording', font=B1, text_color='#979797', justify= 'left')
-cfgM_title2.grid(row=4, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (40,0))
+cfgM_title2.grid(row=4, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (20,0))
 # record audio
 cfgM_recordtxt = customtkinter.CTkLabel(configFrameMain, text= 'Record Audio', font=B1)
 cfgM_recordtxt.grid(row=5, column=0, sticky='w', padx= (40,0), pady= (10,0))
+cfgM_recordtoggleVar = customtkinter.IntVar()
+cfgM_recordtoggle = customtkinter.CTkSwitch(configFrameMain, variable= cfgM_recordtoggleVar, text= 'OFF',
+                                            onvalue= 1, offvalue= 0, command= cfgM_recordtoggleEvent)
+cfgM_recordtoggle.grid(row=5, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # # record duration
 cfgM_durtxt = customtkinter.CTkLabel(configFrameMain, text= 'Audio Record Duration', font=B1)
-cfgM_durtxt.grid(row=6, column=0, sticky='w', padx= (40,0), pady= (0,0))
-cfgM_durentry = customtkinter.CTkEntry(configFrameMain, width= 48)
-cfgM_durentry.grid(row=6, column=2, sticky='w', padx= (0,0), pady= (10,0))
-
+cfgM_durtxt.grid(row= 6, column=0, sticky='w', padx= (40,0), pady= (10,0))
+cfgM_durentry = customtkinter.CTkEntry(configFrameMain, width= 48, state= 'disabled', border_color= '#343638', fg_color= '#2b2b2b')
+cfgM_durentry.grid(row=6, column=2, sticky='w', padx= (10,0), pady= (10,0))
+# stream settings 
+cfgM_title3 = customtkinter.CTkLabel(configFrameMain, text= 'Stream Settings', font=B1, text_color='#979797', justify= 'left')
+cfgM_title3.grid(row=1, column=7, columnspan= 5, sticky='w', padx= (0,0), pady= (40,0))
+# trial averaged gain
+cfgM_trlavgtxt = customtkinter.CTkLabel(configFrameMain, text= 'Trial to Average for ERP', font=B1)
+cfgM_trlavgtxt.grid(row=2, column=7, sticky='w', padx= (0,0), pady= (10,0))
+cfgM_trlavgentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_trlavgentry.grid(row=2, column=9, sticky='w', padx= (10,0), pady= (10,0))
+# epoch min
+cfgM_epmintxt = customtkinter.CTkLabel(configFrameMain, text= 'Epoch Min (s)', font=B1)
+cfgM_epmintxt.grid(row=3, column=7, sticky='w', padx= (0,0), pady= (10,0))
+cfgM_epminentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_epminentry.grid(row=3, column=9, sticky='w', padx= (10,0), pady= (10,0))
+# epoch max
+cfgM_epmaxtxt = customtkinter.CTkLabel(configFrameMain, text= 'Epoch Max (s)', font=B1)
+cfgM_epmaxtxt.grid(row=4, column=7, sticky='w', padx= (0,0), pady= (0,0))
+cfgM_epmaxentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_epmaxentry.grid(row=4, column=9, sticky='w', padx= (10,0), pady= (0,0))
+# info note
+cfgM_infonotestr = 'Note: Please make sure Bela board is connected to the computer to reflect the changes you make in the Bela Board Settings\nsection. After connecing the Bela Board to computer via USB make sure you the drive is visible in My Computer'
+cfgM_infonote = customtkinter.CTkLabel(configFrameMain, text= cfgM_infonotestr, font=B3, text_color= '#5f5f5f', justify= 'left')
+cfgM_infonote.grid(row=8, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (24,0))
 
 # back button
 cfgM_BTback2main = customtkinter.CTkButton(configFrameMain, text= 'Back to Main Menu', fg_color='#5b5b5b', text_color='#b6b6b6', hover_color='#4f4f4f', 
@@ -317,6 +354,75 @@ cfgM_BTback2main.grid(row=9, column=0, sticky='sw', padx= (40,0), pady= (0,40))
 cfgM_BTsave = customtkinter.CTkButton(configFrameMain, text= 'Save Changes',
                                     command= on_cfgsave)
 cfgM_BTsave.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
+
+
+
+
+# --- Streamer Main Frame UI ---
+
+# title 
+strM_title =  customtkinter.CTkLabel(streamerFrameMain, text= 'Stream nEEGlace', font=H2)
+strM_title.grid(row=0, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (40,0))
+# body text '
+strM_bodystr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua.'
+strM_body = customtkinter.CTkLabel(streamerFrameMain, text= bodystr, font=B2, text_color='#979797', justify= 'left')
+strM_body.grid(row=2, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (10,0))
+
+
+# stream info
+strM_title1 = customtkinter.CTkLabel(streamerFrameMain, text= 'Sream Info', font=B1, text_color='#979797', justify= 'left')
+strM_title1.grid(row=3, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (60,0))
+# sfreq
+sfreq = 40
+strM_sfreqtxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Sampling Frequency', font=B1)
+strM_sfreqtxt.grid(row=4, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (10,0))
+strM_sfreqans = customtkinter.CTkLabel(streamerFrameMain, text= f'{sfreq} Hz', font=B1)
+strM_sfreqans.grid(row=4, column=2, columnspan= 5, sticky='w', padx= (0,0), pady= (10,0))
+# channel count
+nchan = 8
+strM_nchantxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Number of Channels', font=B1)
+strM_nchantxt.grid(row=5, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (0,0))
+strM_nchanans = customtkinter.CTkLabel(streamerFrameMain, text= f'{nchan} Channels', font=B1)
+strM_nchanans.grid(row=5, column=2, columnspan= 5, sticky='w', padx= (0,0), pady= (0,0))
+# trigger count
+trgchan = 7
+strM_trgchantxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Trigger Channel', font=B1)
+strM_trgchantxt.grid(row=6, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (0,0))
+strM_trgchanans = customtkinter.CTkLabel(streamerFrameMain, text= f'{trgchan}', font=B1)
+strM_trgchanans.grid(row=6, column=2, columnspan= 5, sticky='w', padx= (0,0), pady= (0,0))
+# audio recording status
+recordstaus = 'On'
+strM_recordtxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Audio Recording', font=B1)
+strM_recordtxt.grid(row=7, column=0, columnspan= 5, sticky='w', padx= (40,0), pady= (0,0))
+strM_recordans = customtkinter.CTkLabel(streamerFrameMain, text= f'{recordstaus}', font=B1)
+strM_recordans.grid(row=7, column=2, columnspan= 5, sticky='w', padx= (0,0), pady= (0,0))
+
+# plot info
+strM_title3 = customtkinter.CTkLabel(streamerFrameMain, text= 'Plot Info', font=B1, text_color='#979797', justify= 'left')
+strM_title3.grid(row=3, column=7, columnspan= 5, sticky='w', padx= (0,0), pady= (60,0))
+# trials averaged
+trlcount = 15
+strM_trlavgtxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Trial Count', font=B1)
+strM_trlavgtxt.grid(row=4, column=7, columnspan= 5, sticky='w', padx= (0,0), pady= (10,0))
+strM_trlavgans = customtkinter.CTkLabel(streamerFrameMain, text= f'{trlcount}', font=B1)
+strM_trlavgans.grid(row=4, column=9, sticky='w', padx= (80,0), pady= (10,0))
+# sound detector
+sndstat = 'Sound Detected'
+strM_sndstattxt = customtkinter.CTkLabel(streamerFrameMain, text= 'Trigger', font=B1)
+strM_sndstattxt.grid(row=5, column=7, columnspan= 5, sticky='w', padx= (0,0), pady= (0,0))
+strM_sndstatans = customtkinter.CTkLabel(streamerFrameMain, text= f'{sndstat}', font=B1, text_color='#2cd756')
+strM_sndstatans.grid(row=5, column=9, columnspan= 5, sticky='w', padx= (80,0), pady= (0,0))
+
+# quit button
+strM_BTquit = customtkinter.CTkButton(streamerFrameMain, text= 'Quit Stream', fg_color='#5b2b2b', text_color='#b6b6b6', hover_color='#4f2121')
+strM_BTquit.grid(row=9, column=0, sticky='sw', padx= (40,0), pady= (0,40))
+# ERP button 
+strM_BTerp = customtkinter.CTkButton(streamerFrameMain, text= 'Show ERP', fg_color='#5b5b5b', text_color='#b6b6b6', hover_color='#4f4f4f')
+strM_BTerp.grid(row=9, column=9, sticky='se', padx= (0,190), pady= (0,40))
+# Plot Stream button
+strM_BTeegstream = customtkinter.CTkButton(streamerFrameMain, text= 'Plot EEG Data', fg_color='#5b5b5b', text_color='#b6b6b6', hover_color='#4f4f4f')
+strM_BTeegstream.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
+
 
 # run app
 app.mainloop()
