121713838a70f0a4f63c1cbb683a42d83ecfaa4a
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-09-04 11:14:08 +0200

    Update nEEGlaceGUI_v1.py

--- nEEGlaceGUI_v1.py
+++ nEEGlaceGUI_v1.py
@@ -14,11 +14,22 @@ python using Tkinter
 
 # params ---------------------------------------------------
 
+# root path
+rootpath = r'C:\Users\messung\Desktop\nEEGlace GUI'
+# config file
+configfilename = 'nEEGlaceConfigfile.txt' 
+
+# threshold for sound detection
+soundThresh = 5
+# trigger channel in the stream 
+triggerChan = 7
+# period to look for sound trigger (in sec)
+look4sound = 5
+
 # common outputs of push2lsl
 errstr1   = 'not recognised as an internal or external command'
 errstr2   = 'DeviceNotFoundError'
 successtr = 'Device info packet has been received. Connection has been established. Streaming...'
-
 
 # ----------------------------------------------------------
 
@@ -34,9 +45,70 @@ import customtkinter
 # subprocess
 import subprocess
 import threading 
+# for ssh connection with Bela
+import paramiko
 # lsl
 from pylsl import StreamInlet, StreamInfo, resolve_stream
 
+# set directory
+os.chdir(r'C:\Users\messung\Desktop\nEEGlace GUI') 
+from belaconnect import checkBelaStatus, getBelaConfig, dumpBelaConfig
+
+# open the config file and fetch data
+configfile = os.path.join(rootpath, configfilename)
+# function to read text file
+def readConfig():
+    with open(configfile, 'r') as f:
+        lines = f.readlines()
+    return lines
+# function to fetch data from config file
+def fetchConfig():   
+    global bela_micgain, bela_thresh, bela_record, bela_recorddur, stream_erpavg, stream_tmin, stream_tmax
+    # read lines from config file
+    configlines = readConfig()
+    # fetching data
+    bela_micgain   = int(configlines[0])
+    bela_thresh    = float(configlines[1])
+    bela_record    = int(configlines[2])
+    bela_recorddur = int(configlines[3])
+    stream_erpavg  = int(configlines[4])
+    stream_tmin    = float(configlines[5])
+    stream_tmax    = float(configlines[6])   
+    return bela_micgain, bela_thresh, bela_record, bela_recorddur, stream_erpavg, stream_tmin, stream_tmax
+
+# function to dump data to config file
+def dumpConfig():
+    with open(configfile, 'w') as f:
+        f.writelines(f'{bela_micgain}\n')
+        f.writelines(f'{bela_thresh}\n')
+        f.writelines(f'{bela_record}\n')
+        f.writelines(f'{bela_recorddur}\n')
+        f.writelines(f'{stream_erpavg}\n')
+        f.writelines(f'{stream_tmin}\n')
+        f.writelines(f'{stream_tmax}\n')
+
+# function to read and update from the Bela config file
+def updateConfig():
+    global belastatus
+    configlines = readConfig()
+    belavalues, belastatus = getBelaConfig()
+    if belastatus:    
+        configlines[0] = f'{belavalues[1]}\n'
+        configlines[1] = f'{belavalues[0]}\n'
+        configlines[2] = f'{belavalues[2]}\n'
+        configlines[3] = f'{belavalues[3]}\n'
+        with open(configfile, 'w') as f:
+            f.writelines(configlines)
+        
+
+# function to dump config settings to Bela board 
+def updateBela():
+    configlines = readConfig()
+    values = [configlines[0], configlines[1], configlines[2], configlines[3]]
+    time.sleep(1)
+    writestatus = dumpBelaConfig(values)
+    return writestatus
+        
 
 
 
@@ -85,11 +157,14 @@ B1 = ('Arial', 14)
 B2 = ('Arial', 12)
 B3 = ('Arial', 10)
 
+# color palette 
+UItextbox = {'active': {'boxbg': '#343638', 'boxborder': '#565b5e', 'boxfont': '#ffffff'},
+                  'deactive': {'boxbg': '#2b2b2b', 'boxborder': '#343638', 'boxfont': '#5f5f5f'}}
+UIfont = {'normal': '#ffffff', 'notes': '#5f5f5f','deactive': '#5f5f5f', 'error': '#f34444', 'success': '#2cd756'}
+
 # configure grid layout 
 app.grid_rowconfigure(0, weight=1)
 app.grid_columnconfigure(0, weight=1)
-
-
 
 
     
@@ -144,6 +219,7 @@ def on_troubleshoot():
 def on_config():
     mainFrame.grid_forget()
     configFrameMain.grid(sticky='nsew')
+    updateConfig()
 def on_start():
     mainFrame.grid_forget()
     streamerFrameMain.grid(sticky='nsew')
@@ -307,7 +383,7 @@ def on_t2next():
 # function to continuously check streamStatus from the thread
 stopCheck = False
 def checkThread(connectionThread):
-    global streams, inlet, srate, stopCheck
+    global streams, inlet, srate, nbchans, stopCheck
     # checks every 100ms if thread is complete
     if connectionThread.is_alive():
         if not stopCheck:    
@@ -326,8 +402,7 @@ def checkThread(connectionThread):
                 troubleshootFrame3.grid(sticky='nsew')
                 stopCheck = True
             except Exception as e:
-                print('Unable to connect to LSL stream')          
-            
+                print('Unable to connect to LSL stream')                     
     # if complete
     else:
         # check connection status
@@ -338,7 +413,7 @@ def checkThread(connectionThread):
         elif streamStatus == 3:
             t2_bar.stop()
             t2_bar.grid_forget()
-            t2_Q1label.configure(text= 'Error: Unable to connect to nEEGlace. Restart and try again. Also kill the explorepy subprocess from Windows Task Manager if exist', text_color='#f34444')    
+            t2_Q1label.configure(text= 'Error: Restart and try again. Also kill the explorepy subprocess from Windows Task Manager if exist', text_color='#f34444')    
         elif streamStatus == 4:
             t2_bar.stop()
             t2_Q1label.configure(text= 'Error: Unable to connect to nEEGlace. Restart and try again.', text_color='#f34444')
@@ -400,26 +475,156 @@ t2_BTnext.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
 
 
 
+# --- troubleshoot frame3 UI ---
+def detectSound():
+    global soundDetector, sample
+    sample = None
+    # pull sample
+    sample, timestamp = inlet.pull_sample(timeout= 1.0)
+    if sample is None:
+        return
+    # check if trigger present 
+    if sample[triggerChan-1] > soundThresh:
+        print('sound detected')
+        soundDetector = True
+
+# button functions
+def on_t3start():
+    # start a new top level window
+    testAudioWindow = customtkinter.CTkToplevel(troubleshootFrame3)
+    testAudioWindow.title('Test Audio')
+    testAudioWindow.geometry('400x200')
+    # restricting window resize
+    testAudioWindow.resizable(False, False)
+    
+    def on_tawclose():
+        testAudioWindow.destroy()
+        testAudioWindow.update()
+    
+    count = 0
+    nosound = 0
+    
+    # while count<3 and nosound<3:  
+    # body text
+    taw_makesound = customtkinter.CTkLabel(testAudioWindow, text= 'Make a Loud', font=B1)
+    taw_makesound.pack(pady=(10,0))
+    taw_feedback = customtkinter.CTkLabel(testAudioWindow, text= '', font=H1, text_color='#2cd756')
+    taw_feedback.pack(pady=(10,0))
+            
+            
+        
+        
+        
+    # close button 
+    taw_BTclose = customtkinter.CTkButton(testAudioWindow, text= 'Close', fg_color='#5b5b5b', text_color='#b6b6b6', hover_color='#4f4f4f',
+                                          command= on_tawclose)
+    taw_BTclose.pack(pady= (100,0))
+    
+
+# title 
+t3_title =  customtkinter.CTkLabel(troubleshootFrame3, text= 'Troubleshoot nEEGlace', font=H2)
+t3_title.grid(row=0, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (40,0))
+# device status
+t3_devicestat = customtkinter.CTkLabel(troubleshootFrame3, text= 'Device Status:', font=B2, text_color='#979797', justify= 'left')
+t3_devicestat.grid(row=1, column=0, sticky='w', padx= (40,0), pady= (2,0))
+t3_devicestatans = customtkinter.CTkLabel(troubleshootFrame3, text= 'ON', font=B2, text_color='#2cd756', justify= 'left')
+t3_devicestatans.grid(row=1, column=0, sticky='w', padx= (125,0), pady= (2,0))
+t3_ampstat = customtkinter.CTkLabel(troubleshootFrame3, text= 'Amplifier Status:', font=B2, text_color='#979797', justify= 'left')
+t3_ampstat.grid(row=1, column=1, sticky='w', padx= (0,0), pady= (2,0))
+t3_ampstatans = customtkinter.CTkLabel(troubleshootFrame3, text= 'Streaming', font=B2, text_color='#2cd756', justify= 'left')
+t3_ampstatans.grid(row=1, column=1, sticky='w', padx= (100,0), pady= (2,0))
+# body font
+t3_body1 = customtkinter.CTkLabel(troubleshootFrame3, text= 'Step 2', font=H3, justify= 'left')
+t3_body1.grid(row=2, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (0,0))
+t3_bodystr = 'Now lets test the sound stream to check\nif nEEGlace is able to detect the audio'
+t3_body2 = customtkinter.CTkLabel(troubleshootFrame3, text= t2_bodystr, font=B1, justify= 'left')
+t3_body2.grid(row=2, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (60,0))
+# instructions
+t3_Q1str = 'Click on the Start Testing Audio button below and make loud \nsounds to test the audio detection'
+t3_Q1 = customtkinter.CTkLabel(troubleshootFrame3, text= t3_Q1str, font=B1, text_color='#979797', justify= 'left')
+t3_Q1.grid(row=3, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (40,0))
+
+# quit button
+t3_BTquit = customtkinter.CTkButton(troubleshootFrame3, text= 'Quit Stream', fg_color='#5b2b2b', text_color='#b6b6b6', hover_color='#4f2121')
+t3_BTquit.grid(row=9, column=0, sticky='sw', padx= (40,0), pady= (0,40))
+# start button
+t3_BTstartsoundcheck = customtkinter.CTkButton(troubleshootFrame3, text= 'Start Testing Audio',
+                                               command= on_t3start)
+t3_BTstartsoundcheck.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
+
+
+
+
 # --- Configure Main Frame UI ---
 
 # button functions
-def on_cfgmsave():
+def on_cfgmback():
     configFrameMain.grid_forget()
     mainFrame.grid(sticky='nsew')
+    
+def on_cfgmsave():
+    global bela_micgain, bela_thresh, bela_record, bela_recorddur, stream_erpavg, stream_tmin, stream_tmax, belawritestatus
+    bela_micgain   = cfgM_gainentry.get() 
+    bela_thresh    = cfgM_threshentry.get()
+    bela_record    = cfgM_recordtoggleVar.get()
+    bela_recorddur = cfgM_durentry.get()
+    stream_erpavg  = cfgM_trlavgentry.get()
+    stream_tmin    = cfgM_epminentry.get()
+    stream_tmax    = cfgM_epmaxentry.get()
+    
+    # check input conditions
+    if int(bela_micgain) > 55:
+       bela_micgain = '55' 
+       cfgM_gainentry.delete(0, 'end')
+       cfgM_gainentry.insert(0, bela_micgain)
+       tkinter.messagebox.showwarning("Warning", "Microphone gain exceeds the maximum allowed value of 55. It has been reset to 55.")
+    
+    if int(bela_recorddur) > 720:
+       bela_recorddur = '720' 
+       cfgM_durentry.delete(0, 'end')
+       cfgM_durentry.insert(0, bela_recorddur)
+       tkinter.messagebox.showwarning("Warning", "Recorde duration exceeds the maximum allowed value of 720sec. It has been reset to 720sec.")
+    
+    
+    
+    
+    # write data to config file
+    dumpConfig()
+    # update bela
+    belastatus = checkBelaStatus()
+    if belastatus:    
+        belawritestatus = updateBela()
+        if not belawritestatus:
+            cfgM_infonotestr = 'Error saving to Bela Board. Verify the connection and try again'
+            cfgM_infonote.configure(text = cfgM_infonotestr, text_color= UIfont['error'])
+            
+        else:
+            cfgM_infonotestr = 'Changes made to the settings are saved'
+            cfgM_infonote.configure(text = cfgM_infonotestr, text_color= UIfont['success'])
+            
+            # Switch frames after 2 seconds without freezing the GUI
+            configFrameMain.after(1400, lambda: (configFrameMain.grid_forget(), mainFrame.grid(sticky='nsew')))
+    else:
+        cfgM_infonotestr = 'Changes made to the settings are saved'
+        cfgM_infonote.configure(text = cfgM_infonotestr, text_color= UIfont['success'])
+        
+        # Switch frames after 2 seconds without freezing the GUI
+        configFrameMain.after(1400, lambda: (configFrameMain.grid_forget(), mainFrame.grid(sticky='nsew')))
+    
+    
+    
 def cfgM_recordtoggleEvent():
     if cfgM_recordtoggleVar.get() == 1:
-        cfgM_durentry.configure(state= 'normal', border_color= '#565b5e', fg_color= '#343638')
+        cfgM_durentry.configure(state= 'normal', border_color= '#565b5e', fg_color= '#343638', text_color= UItextbox['active']['boxfont'])
         cfgM_recordtoggle.configure(text= 'ON')
     if cfgM_recordtoggleVar.get() == 0:
-        cfgM_durentry.configure(state= 'disabled', border_color= '#343638', fg_color= '#2b2b2b')
+        cfgM_durentry.configure(state= 'disabled', border_color= '#343638', fg_color= '#2b2b2b', text_color= UItextbox['deactive']['boxfont'])
         cfgM_recordtoggle.configure(text= 'OFF')
         
-def on_cfgsave():
-    print(f'Gain      : {cfgM_gainentry.get()}')
-    print(f'Threshold : {cfgM_threshentry.get()}')
-    if cfgM_recordtoggleVar.get() == 1:
-        print(f'Record Duration : {cfgM_durentry.get()}')
         
+# fetch config data
+updateConfig()
+configdata = fetchConfig()
 
 # title 
 cfgM_title =  customtkinter.CTkLabel(configFrameMain, text= 'Configure nEEGlace', font=H2)
@@ -431,11 +636,13 @@ cfgM_title1.grid(row=1, column=0, columnspan= 5, sticky='w', padx= (40,0), pady=
 cfgM_gaintxt = customtkinter.CTkLabel(configFrameMain, text= 'Microphone Input Gain', font=B1)
 cfgM_gaintxt.grid(row=2, column=0, sticky='w', padx= (40,0), pady= (10,0))
 cfgM_gainentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_gainentry.insert(0, configdata[0])
 cfgM_gainentry.grid(row=2, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # energy threshold
 cfgM_threshtxt = customtkinter.CTkLabel(configFrameMain, text= 'Energy Threshold', font=B1)
 cfgM_threshtxt.grid(row=3, column=0, sticky='w', padx= (40,0), pady= (0,0))
 cfgM_threshentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_threshentry.insert(0, configdata[1])
 cfgM_threshentry.grid(row=3, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # audio setings
 cfgM_title2 = customtkinter.CTkLabel(configFrameMain, text= 'Audio Recording', font=B1, text_color='#979797', justify= 'left')
@@ -444,13 +651,30 @@ cfgM_title2.grid(row=4, column=0, columnspan= 5, sticky='w', padx= (40,0), pady=
 cfgM_recordtxt = customtkinter.CTkLabel(configFrameMain, text= 'Record Audio', font=B1)
 cfgM_recordtxt.grid(row=5, column=0, sticky='w', padx= (40,0), pady= (10,0))
 cfgM_recordtoggleVar = customtkinter.IntVar()
-cfgM_recordtoggle = customtkinter.CTkSwitch(configFrameMain, variable= cfgM_recordtoggleVar, text= 'OFF',
+cfgM_recordtoggleVar.set(configdata[2])
+# setting values based on record status 
+if cfgM_recordtoggleVar.get() == 1:
+    toggletext = 'ON'
+    dur_state = 'normal'
+    dur_border = UItextbox['active']['boxborder']
+    dur_fg = UItextbox['active']['boxbg']
+    dur_txtclr = UItextbox['active']['boxfont']
+elif cfgM_recordtoggleVar.get() == 0:
+    toggletext = 'OFF'
+    dur_state = 'disabled'
+    dur_border = UItextbox['deactive']['boxborder']
+    dur_fg = UItextbox['deactive']['boxbg']
+    dur_txtclr = UItextbox['deactive']['boxfont']
+cfgM_recordtoggle = customtkinter.CTkSwitch(configFrameMain, variable= cfgM_recordtoggleVar, text= toggletext,
                                             onvalue= 1, offvalue= 0, command= cfgM_recordtoggleEvent)
 cfgM_recordtoggle.grid(row=5, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # # record duration
-cfgM_durtxt = customtkinter.CTkLabel(configFrameMain, text= 'Audio Record Duration', font=B1)
+cfgM_durtxt = customtkinter.CTkLabel(configFrameMain, text= 'Record Duration (s)', font=B1)
 cfgM_durtxt.grid(row= 6, column=0, sticky='w', padx= (40,0), pady= (10,0))
-cfgM_durentry = customtkinter.CTkEntry(configFrameMain, width= 48, state= 'disabled', border_color= '#343638', fg_color= '#2b2b2b')
+cfgM_durentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_durentry.insert(0, configdata[3])
+cfgM_durentry.configure(state= dur_state, border_color= dur_border, fg_color= dur_fg, text_color= dur_txtclr)
+
 cfgM_durentry.grid(row=6, column=2, sticky='w', padx= (10,0), pady= (10,0))
 # stream settings 
 cfgM_title3 = customtkinter.CTkLabel(configFrameMain, text= 'Stream Settings', font=B1, text_color='#979797', justify= 'left')
@@ -459,30 +683,52 @@ cfgM_title3.grid(row=1, column=7, columnspan= 5, sticky='w', padx= (0,0), pady= 
 cfgM_trlavgtxt = customtkinter.CTkLabel(configFrameMain, text= 'Trial to Average for ERP', font=B1)
 cfgM_trlavgtxt.grid(row=2, column=7, sticky='w', padx= (0,0), pady= (10,0))
 cfgM_trlavgentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_trlavgentry.insert(0, configdata[4])
 cfgM_trlavgentry.grid(row=2, column=9, sticky='w', padx= (10,0), pady= (10,0))
 # epoch min
 cfgM_epmintxt = customtkinter.CTkLabel(configFrameMain, text= 'Epoch Min (s)', font=B1)
 cfgM_epmintxt.grid(row=3, column=7, sticky='w', padx= (0,0), pady= (10,0))
 cfgM_epminentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_epminentry.insert(0, configdata[5])
 cfgM_epminentry.grid(row=3, column=9, sticky='w', padx= (10,0), pady= (10,0))
 # epoch max
 cfgM_epmaxtxt = customtkinter.CTkLabel(configFrameMain, text= 'Epoch Max (s)', font=B1)
 cfgM_epmaxtxt.grid(row=4, column=7, sticky='w', padx= (0,0), pady= (0,0))
 cfgM_epmaxentry = customtkinter.CTkEntry(configFrameMain, width= 48)
+cfgM_epmaxentry.insert(0, configdata[6])
 cfgM_epmaxentry.grid(row=4, column=9, sticky='w', padx= (10,0), pady= (0,0))
+
 # info note
-cfgM_infonotestr = 'Note: Please make sure Bela board is connected to the computer to reflect the changes you make in the Bela Board Settings\nsection. After connecing the Bela Board to computer via USB make sure you the drive is visible in My Computer'
-cfgM_infonote = customtkinter.CTkLabel(configFrameMain, text= cfgM_infonotestr, font=B3, text_color= '#5f5f5f', justify= 'left')
+cfgM_infonotestr = ''
+cfgM_infonote = customtkinter.CTkLabel(configFrameMain, text= cfgM_infonotestr, font=B2, text_color= UIfont['notes'], justify= 'left')
 cfgM_infonote.grid(row=8, column=0, columnspan= 10, sticky='w', padx= (40,0), pady= (24,0))
 
 # back button
 cfgM_BTback2main = customtkinter.CTkButton(configFrameMain, text= 'Back to Main Menu', fg_color='#5b5b5b', text_color='#b6b6b6', hover_color='#4f4f4f', 
-                                 command= on_cfgmsave)
+                                 command= on_cfgmback)
 cfgM_BTback2main.grid(row=9, column=0, sticky='sw', padx= (40,0), pady= (0,40))
+
 # save button
 cfgM_BTsave = customtkinter.CTkButton(configFrameMain, text= 'Save Changes',
-                                    command= on_cfgsave)
+                                    command= on_cfgmsave)
 cfgM_BTsave.grid(row=9, column=9, sticky='se', padx= (10,40), pady= (0,40))
+
+if not belastatus:
+    cfgM_gaintxt.configure(text_color= UIfont['deactive'])
+    cfgM_gainentry.configure(state= 'disabled', border_color= UItextbox['active']['boxborder'], fg_color= UItextbox['deactive']['boxbg'], text_color= UItextbox['deactive']['boxfont'])
+    cfgM_threshtxt.configure(text_color= UIfont['deactive'])
+    cfgM_threshentry.configure(state= 'disabled', border_color= UItextbox['active']['boxborder'], fg_color= UItextbox['deactive']['boxbg'], text_color= UItextbox['deactive']['boxfont'])
+    cfgM_recordtxt.configure(text_color= UIfont['deactive'])
+    cfgM_recordtoggle.configure(state= 'disabled', fg_color= UItextbox['deactive']['boxborder'])
+    cfgM_durtxt.configure(text_color= UIfont['deactive'])
+    cfgM_durentry.configure(state= 'disabled', border_color= UItextbox['active']['boxborder'], fg_color= UItextbox['deactive']['boxbg'], text_color= UItextbox['deactive']['boxfont'])
+    # info note
+    cfgM_infonotestr = 'NOTE: Bela board is not connected to the computer. To make changes to the Bela Board Settings, Connect Bela Board \nto computer via USB and click Connect Bela.'
+    cfgM_infonote.configure(text = cfgM_infonotestr)
+    # connect bella button
+    cfgM_BTconnectbela = customtkinter.CTkButton(configFrameMain, text= 'Connect Bela', fg_color='#ffffff', text_color='#000000', hover_color='#979797',
+                                       command= on_config)
+    cfgM_BTconnectbela.grid(row=9, column=2, sticky='sw', padx= (0,10), pady= (0,40))
 
 
 
